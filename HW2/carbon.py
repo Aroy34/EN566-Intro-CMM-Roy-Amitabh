943714186e19f0da47acf3fdfa0c4decce38a8cc
Aroy34 <143626375+Aroy34@users.noreply.github.com>
2023-09-30 00:05:07 -0400

    working code
    Code is now 60% operationsal few things 1) need to comapre the activity value 2) need to add the magnus value 3) add makefile 4) Write a latex with derivation, my logic for the code,bibliograpy,contributions etc.

--- HW2/carbon.py
+++ HW2/carbon.py
@@ -1,64 +1,55 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import math
+import argparse as argp
+
+def beta_decay():
+    psr = argp.ArgumentParser("Activity")
+    psr.add_argument('--plot', type=str, default=0,
+                     help="enter the step widths ','")
+    arg = psr.parse_args()
+    step_ = arg.plot.split(",")
+    step_size = [int(stp) for stp in step_]
+    # Initial number of particles
+    initial_particles = (10**-12/14)*6.022*10**23
+    # t(1/2) half life in years
+    half_life = 5700
+    # Decay constant
+    tau = -half_life/math.log(0.5)
+    print(tau)
+    time = []
+    number_particle_at_t =[]
+    
+    for i in range(len(step_size)):
+        del time[:]
+        del number_particle_at_t[:]
+        number_particle_at_t.append(initial_particles)
+        time.append(0)
+
+        for t in range(0,20000+step_size[i],step_size[i]):
+            if t == 0:
+                number_particle_at_t.append(initial_particles)
+            else:
+                index = int((t)/step_size[i])
+                number_particle_at_t.append(number_particle_at_t[index]-(1/tau*number_particle_at_t[index]*step_size[i]))
+
+        for y in range(0,20000+step_size[i],step_size[i]):
+            if y == 0:
+                time.append(y)
+            else:
+                time.append(y)
+
+        plt.plot(time, number_particle_at_t, label=" Step_Width = "+step_[i])
+
+    plt.xlabel("Time (Years)")
+    plt.ylabel("No of Particles (N * 10^5)")
+    plt.legend()
+    plt.title("Acivity of _____")
+    plt.show()
 
 
-# Initial number of particles
-initial_particles = (10**-12/14)*6.022*10**23
-# t(1/2) half life in years
-half_life = 5700
-# Decay constant
-tau = -half_life/math.log(0.5)
-print(tau)
-
-# Defining the memory allocation
-dt = 10
-dt2=100
-dt3=1000
-
-time = []
-time2 = []
-time3 = []
-number_particle_at_t =[]
-number_particle_at_t.append(10**-12/14*6.022*10**23)
-
-number_particle =[]
-number_particle.append(10**-12/14*6.022*10**23),
-
-number =[]
-number.append(10**-12/14*6.022*10**23),
-
-
-for t in range(0,20000-dt,dt):
-    index = int((t)/dt)
-    number_particle_at_t.append(number_particle_at_t[index]-(1/tau*number_particle_at_t[index]*dt))
-
-#print(len(number_particle_at_t))
-for t in range(0,20000-dt2,dt2):
-    index = int((t)/dt2)
-    number_particle.append(number_particle[index]-(1/tau*number_particle[index]*dt2))
-
-for t in range(0,20000-dt3,dt3):
-    index = int((t)/dt3)
-    number.append(number[index]-(1/tau*number[index]*dt3))
-
-
-for y in range(0,20000,dt):
-    time.append(y)
-
-for y2 in range(0,20000,dt2):
-    time2.append(y2)
-
-for y3 in range(0,20000,dt3):
-    time3.append(y3)
-
-plt.plot(time, number_particle_at_t)
-plt.plot(time2, number_particle)
-plt.plot(time3, number)
-plt.show()
-
-
-
+if __name__ == "__main__":
+    beta_decay()
 
 
 
--- HW2/golf.py
+++ HW2/golf.py
@@ -1,92 +1,124 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import math
-import argparse as argp 
+import argparse as argp
 
 
 
-'''psr = argp.ArgumentParser("Projectile Trajectory")
-psr.add_argument('--plot', type=str, default=0,
-                 help="enter the angle seperated by ','")
-arg = psr.parse_args()'''
-
-v = 70
-dt = 0.1
+v = 40
+dt = 0.005
 x_trajec = []
-y_trajec =[]
+y_trajec = []
 x_trajec_d = []
-y_trajec_d =[]
-theta = [34,45,9]
+y_trajec_d = []
+x_trajec_dd = []
+y_trajec_dd = []
 g = 9.81
-vx =[]
+vx = []
 vy = []
-vx_d =[]
+vx_d = []
 vy_d = []
+vx_dd = []
+vy_dd = []
 c = 0.5
+c_dim = 7/v
 d = 1.29
 A = 0.0014
 m = 0.046
 Fdrag = -1*c*d*A*v**2
- 
-
-for l in range(3):
-    for k in range(len(theta)):
-        if l == 0:
-            del x_trajec[:]
-            del y_trajec[:]
-            del vx[:]
-            del vy[:]
-            x_trajec.append(0)
-            y_trajec.append(0)
-            vx.append(v*math.cos(math.radians(theta[k])))
-            vy.append(v*math.sin(math.radians(theta[k])))
-            i=0
-            while True:
-                x_trajec.append(x_trajec[i]+vx[i]*dt)
-                vx.append(vx[i])
-
-                y_trajec.append(y_trajec[i]+vy[i]*dt)
-                vy.append(vy[i]-g*dt)
-
-                if y_trajec[i+1] <= 0:
-                    break
-
-                i=i+1
-            plt.plot(x_trajec, y_trajec,label = str(theta[k])+"°")
-            plt.xlabel("Distance (m)")
-            plt.ylabel("Height (m)")
-            plt.legend()
-            plt.title("Trajectory")
-                  
-        if l == 1:
-            del x_trajec_d[:]
-            del y_trajec_d[:]
-            del vx_d[:]
-            del vy_d[:]
-            x_trajec_d.append(0)
-            y_trajec_d.append(0)
-            vx_d.append(v*math.cos(math.radians(theta[k])))
-            vy_d.append(v*math.sin(math.radians(theta[k])))
-            i=0
-            while True:    
-                x_trajec_d.append(x_trajec_d[i]+vx_d[i]*dt)
-                print(x_trajec_d)
-                vx_d.append(vx_d[i]-(A*math.sqrt(vx_d[i]+vy_d[i])*vx_d[i]*dt)/m)
-                y_trajec_d.append(y_trajec_d[i]+vy_d[i]*dt)
-                vy_d.append(vy_d[i]-g*dt-A*math.sqrt(vx_d[i]+vy_d[i])*vx_d[i]*dt/m)
-                print(vy_d)
-
-                if y_trajec_d[i+1] <= 0:
-                    break
-
-                i=i+1
-
-            plt.plot(x_trajec_d, y_trajec_d,label = str(theta[k])+"°"+"W/ Drag")
-            plt.xlabel("Distance (m)")
-            plt.ylabel("Height (m)")
-            plt.legend()
-            plt.title("Trajectory with drag")
-
-plt.show()
+Fdrag_dim = -1*c_dim*d*A*v**2
 
 
+def golf_trajectory():
+    psr = argp.ArgumentParser("Golf_Trajectory")
+    psr.add_argument('--plot', type=str, default=0,
+                     help="enter the value of theta seperated by ','")
+    arg = psr.parse_args()
+    angle = arg.plot.split(",")
+    theta = [int(angl) for angl in angle] # check this again
+
+    for l in range(3):
+        for k in range(len(theta)):
+            if l == 0:
+                del x_trajec[:]
+                del y_trajec[:]
+                del vx[:]
+                del vy[:]
+                x_trajec.append(0)
+                y_trajec.append(0)
+                vx.append(v*math.cos(math.radians(theta[k])))
+                vy.append(v*math.sin(math.radians(theta[k])))
+                i = 0
+                while True:
+                    x_trajec.append(x_trajec[i]+vx[i]*dt)
+                    vx.append(vx[i])
+
+                    y_trajec.append(y_trajec[i]+vy[i]*dt)
+                    vy.append(vy[i]-g*dt)
+
+                    if y_trajec[i+1] <= 0:
+                        break
+
+                    i = i+1
+
+                plt.plot(x_trajec, y_trajec, label=str(theta[k])+"°"+"(Ideal)")
+
+            if l == 1:
+                
+                del x_trajec_d[:]
+                del y_trajec_d[:]
+                del vx_d[:]
+                del vy_d[:]
+                x_trajec_d.append(0)
+                y_trajec_d.append(0)
+                vx_d.append(v*math.cos(math.radians(theta[k])))
+                vy_d.append(v*math.sin(math.radians(theta[k])))
+                i = 0
+                while True:
+                    x_trajec_d.append(x_trajec_d[i]+vx_d[i]*dt)
+                    vx_d.append(
+                        vx_d[i]+(Fdrag*math.cos(math.radians(theta[k]))*dt)/m)
+                    y_trajec_d.append(y_trajec_d[i]+vy_d[i]*dt)
+                    vy_d.append(vy_d[i]-g*dt+Fdrag*math.sin(math.radians(theta[k]))*dt/m)
+
+                    if y_trajec_d[i+1] <= 0:
+                        break
+                    i = i+1
+                
+                plt.plot(x_trajec_d[0:i-1], y_trajec_d[0:i-1],
+                         label=str(theta[k])+"°"+"W/ Drag")
+
+            
+            if l == 2:
+                del x_trajec_dd[:]
+                del y_trajec_dd[:]
+                del vx_dd[:]
+                del vy_dd[:]
+                x_trajec_dd.append(0)
+                y_trajec_dd.append(0)
+                vx_dd.append(v*math.cos(math.radians(theta[k])))
+                vy_dd.append(v*math.sin(math.radians(theta[k])))
+                i = 0
+                while True:
+                    x_trajec_dd.append(x_trajec_dd[i]+vx_dd[i]*dt)
+                    vx_dd.append(
+                        vx_dd[i]+(Fdrag_dim*math.cos(math.radians(theta[k]))*dt)/m)
+                
+                    y_trajec_dd.append(y_trajec_dd[i]+vy_dd[i]*dt)
+                    vy_dd.append(vy_dd[i]-g*dt+Fdrag_dim*math.sin(math.radians(theta[k]))*dt/m)
+                    if y_trajec_dd[i+1] <= 0:
+                        break
+                    i = i+1
+
+                plt.plot(x_trajec_dd[0:i-1], y_trajec_dd[0:i-1],
+                         label=str(theta[k])+"°"+"Dimpled + Drag")
+
+    plt.xlabel("Distance (m)")
+    plt.ylabel("Height (m)")
+    plt.legend()
+    plt.title("Trajectory of golf ball")
+    plt.show()
+
+
+if __name__ == "__main__":
+    golf_trajectory()
