92ac7a81cf0f9f57862445ffffb2dc87f68286a8
Aroy34 <143626375+Aroy34@users.noreply.github.com>
2023-10-02 01:47:33 -0400

    Found small bug
    Small bug was related to the magnitude of the velocity

--- HW2/carbon.py
+++ HW2/carbon.py
@@ -15,36 +15,61 @@ def beta_decay():
     # t(1/2) half life in years
     half_life = 5700
     # Decay constant
+    half_life_2 = 5700*2
     tau = -half_life/math.log(0.5)
-    print(tau)
     time = []
     number_particle_at_t =[]
+    rate = []
     
     for i in range(len(step_size)):
         del time[:]
         del number_particle_at_t[:]
-        number_particle_at_t.append(initial_particles)
-        time.append(0)
+        del rate[:]
 
-        for t in range(0,20000+step_size[i],step_size[i]):
+        for t in range(0,20000,step_size[i]):
             if t == 0:
                 number_particle_at_t.append(initial_particles)
+                time.append(t)
             else:
                 index = int((t)/step_size[i])
-                number_particle_at_t.append(number_particle_at_t[index]-(1/tau*number_particle_at_t[index]*step_size[i]))
+                number_particle_at_t.append(number_particle_at_t[index-1]-(1/tau*number_particle_at_t[index-1]*step_size[i]))
+                #print(number_particle_at_t[index],number_particle_at_t[index-1])
+                rate.append(-(number_particle_at_t[index]-number_particle_at_t[index-1])/step_size[i])
+                time.append(t)
+            if step_size[i] == 1000: 
+                no_2nd_order = []
+                time_2nd_order =[]
+                rate_2nd_order = []
+                for t in range(0,20000,step_size[i]):
+                    if t == 0:
+                        no_2nd_order.append(initial_particles)
+                        time_2nd_order.append(t)
+                    else:
+                        index = int((t)/step_size[i])
+                        no_2nd_order.append(no_2nd_order[index-1]-(1/tau*no_2nd_order[index-1]*step_size[i])+(1/tau**2*no_2nd_order[index-1]*step_size[i]**2)/2)
+                        #print(number_particle_at_t[index],number_particle_at_t[index-1])
+                        rate_2nd_order.append(-(no_2nd_order[index]-no_2nd_order[index-1])/step_size[i])
+                        time_2nd_order.append(t)
+                    
 
-        for y in range(0,20000+step_size[i],step_size[i]):
-            if y == 0:
-                time.append(y)
-            else:
-                time.append(y)
 
-        plt.plot(time, number_particle_at_t, label=" Step_Width = "+step_[i])
+        plt.plot(time[:-1], rate, label=" Step_Width = "+step_[i])
+        if step_size[i] == 1000: 
+            plt.plot(time_2nd_order[:-1], rate_2nd_order, label=" Step_Width = "+step_[i]+ " 2nd order considered")
+        rate_interp = round(np.interp(half_life_2, time[:-1], rate),2) 
+        print("Rate at "+ str(half_life_2) +" years = "+ str(rate_interp) + " ;for step width = "+ str(step_size[i]))
+        if step_size[i] == 1000: 
+            rate_interp = round(np.interp(half_life_2, time_2nd_order[:-1], rate_2nd_order),2)
+            print("Rate at "+ str(half_life_2) +" years = "+ str(rate_interp) + " ;for step width = "+ str(step_size[i])+" considering the 2nd order") 
 
+       
+
+    # print(number_particle_at_t)
     plt.xlabel("Time (Years)")
-    plt.ylabel("No of Particles (N * 10^5)")
+    plt.ylabel("Rate * 10^6 (Number of particles/years)")
     plt.legend()
-    plt.title("Acivity of _____")
+    plt.title("Rate of Radioactive Decay,")
+    plt.savefig("Carbon_plot.jpeg")
     plt.show()
 
 
--- HW2/golf.py
+++ HW2/golf.py
@@ -64,7 +64,6 @@ def golf_trajectory():
                     i = i+1
 
                 plt.plot(x_trajec[0:i-1], y_trajec[0:i-1],':', label=str(theta[k])+"°"+"(Ideal)")
-                # print(y_trajec[i],y_trajec[i-1],y_trajec[i-2])
 
             if l == 1:
                 
@@ -80,16 +79,16 @@ def golf_trajectory():
                 while True:
                     x_trajec_d.append(x_trajec_d[i]+vx_d[i]*dt)
                     vx_d.append(
-                        vx_d[i]+(-1*0.5*d*A*vx_d[i]**2*dt)/m)
+                        vx_d[i]+(-1*0.5*d*A*math.sqrt(vx_d[i]**2+vy_d[i]**2)*vx_d[i]*dt)/m)
                     y_trajec_d.append(y_trajec_d[i]+vy_d[i]*dt)
-                    vy_d.append(vy_d[i]-g*dt+(-1*0.5*d*A*vx_d[i]**2*dt)/m)
+                    vy_d.append(vy_d[i]-g*dt+(-1*0.5*d*A*math.sqrt(vx_d[i]**2+vy_d[i]**2)*vx_d[i]*dt)/m)
 
                     if y_trajec_d[i+1] <= 0:
                         break
                     i = i+1
                 
                 plt.plot(x_trajec_d[0:i-1], y_trajec_d[0:i-1],'-.',
-                         label=str(theta[k])+"°"+"W/ Drag")
+                         label=str(theta[k])+"°"+"(Drag)")
                 # print(y_trajec_d[i],y_trajec_d[i-1],y_trajec_d[i-2])
 
 
@@ -107,25 +106,21 @@ def golf_trajectory():
                 while True:
                     x_trajec_dd.append(x_trajec_dd[i]+vx_dd[i]*dt)
                     y_trajec_dd.append(y_trajec_dd[i]+vy_dd[i]*dt)
-                    if abs(vx_dd[i]) >14:
+                    if math.sqrt(vx_dd[i]**2+vy_dd[i]**2) >14:
                         vx_dd.append(vx_dd[i]+(-1*7*d*A*vx_dd[i]*dt)/m)
-                    else: 
-                        vx_dd.append(vx_dd[i]+(-1*0.5*d*A*vx_dd[i]**2*dt)/m)
-                    
-                    if abs(vy_dd[i]) >14:
                         vy_dd.append(vy_dd[i]-g*dt+(-1*7*d*A*vy_dd[i]*dt)/m)
                     else: 
-                        vy_dd.append(vy_dd[i]-g*dt+(-1*0.5*d*A*vy_dd[i]**2*dt)/m)
+                        vx_dd.append(vx_dd[i]+(-1*0.5*d*A*math.sqrt(vx_dd[i]**2+vy_dd[i]**2)*vx_dd[i]*dt)/m)
+                        vy_dd.append(vy_dd[i]-g*dt+(-1*0.5*d*A*math.sqrt(vx_dd[i]**2+vy_dd[i]**2)*vy_dd[i]*dt)/m)
                     
                     if y_trajec_dd[i+1] <= 0:
                         break
                     i = i+1
 
-
                 plt.plot(x_trajec_dd[0:i-1], y_trajec_dd[0:i-1],'--',
-                         label=str(theta[k])+"°"+"Dimpled + Drag")
-                plt.annotate(f'({x_trajec_dd[i-1]:.2f}, {y_trajec_dd[i-1]:.2f})', (x_trajec_dd[i-1], y_trajec_dd[i-1]), fontsize=3,xytext=(-5, 5), textcoords='offset points',
-                 arrowprops=dict(arrowstyle='->', connectionstyle='arc3,rad=0.5'))
+                         label=str(theta[k])+"°"+"(Dimpled + Drag)")
+                # plt.annotate(f'({x_trajec_dd[i-1]:.2f}, {y_trajec_dd[i-1]:.2f})', (x_trajec_dd[i-1], y_trajec_dd[i-1]), fontsize=3,xytext=(-5, 5), textcoords='offset points',
+                #  arrowprops=dict(arrowstyle='->', connectionstyle='arc3,rad=0.5'))
                 # print(y_trajec_dd[i],y_trajec_dd[i-1],y_trajec_dd[i-2])
 
             if l == 3:
@@ -141,28 +136,28 @@ def golf_trajectory():
                 while True:
                     x_trajec_dds.append(x_trajec_dds[i]+vx_dds[i]*dt)
                     y_trajec_dds.append(y_trajec_dds[i]+vy_dds[i]*dt)
-                    if abs(vx_dds[i]) >14:
+                    if math.sqrt(vx_dds[i]**2+vy_dds[i]**2) >14:
                         vx_dds.append(vx_dds[i]+(-1*7*d*A*vx_dds[i]*dt)/m-0.25*vy_dds[i]*dt)
-                    else: 
-                        vx_dds.append(vx_dds[i]+(-1*0.5*d*A*vx_dds[i]**2*dt)/m-0.25*vy_dds[i]*dt)
-                    
-                    if abs(vy_dds[i]) >14:
                         vy_dds.append(vy_dds[i]-g*dt+(-1*7*d*A*vy_dds[i]*dt)/m+0.25*vx_dds[i]*dt)
                     else: 
-                        vy_dds.append(vy_dds[i]-g*dt+(-1*0.5*d*A*vy_dds[i]**2*dt)/m+0.25*vx_dds[i]*dt)
-                    
+                        vx_dds.append(vx_dds[i]+(-1*0.5*d*A*math.sqrt(vx_dds[i]**2+vy_dds[i]**2)*vx_dds[i]*dt)/m-0.25*vy_dds[i]*dt*dt)
+                        vy_dds.append(vy_dds[i]-g*dt+(-1*0.5*d*A*vy_dds[i]*dt)/m+0.25*vx_dds[i]*dt)
+        
+        
                     if y_trajec_dds[i+1] <= 0:
                         break
                     i = i+1
 
 
                 plt.plot(x_trajec_dds[0:i-1], y_trajec_dds[0:i-1],'-',
-                         label=str(theta[k])+"°"+"Spin + Dimpled + Drag")
+                         label=str(theta[k])+"°"+"(Spin + Dimpled + Drag)")
+
 
     plt.xlabel("Distance (m)")
     plt.ylabel("Height (m)")
-    plt.legend()
+    plt.legend(loc='best')
     plt.title("Trajectory of golf ball")
+    plt.savefig("Golf_Trajectory.jpeg")
     plt.show()
     
 if __name__ == "__main__":
