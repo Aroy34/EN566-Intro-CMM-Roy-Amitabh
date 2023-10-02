941a7b270d64ecabb4c733430120a487c2f1b5ec
Aroy34 <143626375+Aroy34@users.noreply.github.com>
2023-10-02 09:30:30 -0400

    All files are there

--- HW2/carbon.py
+++ HW2/carbon.py
@@ -1,70 +1,92 @@
+# Import matplotlib.pyplot for plotting the graph
+# import numpy for the interpolation
+# Import math for doing mathermatical operations
+# Import argparse for passing arguments from the terminal to python code
 import numpy as np
 import matplotlib.pyplot as plt
 import math
 import argparse as argp
 
+
 def beta_decay():
+    """ This functions take argument --plot=step_width.The code calculate
+    the rate of radioactive decay  plot it for the different step_width.
+    Code also interpolates the value of rate at 2 times of half life.
+    Code will also caluclate the condition when the 2nd order of taylor 
+    series is taken into consideration for the larger step width """
     psr = argp.ArgumentParser("Activity")
     psr.add_argument('--plot', type=str, default=0,
                      help="enter the step widths ','")
     arg = psr.parse_args()
     step_ = arg.plot.split(",")
-    step_size = [int(stp) for stp in step_]
+    step_size = [int(stp) for stp in step_]  # list fo all the step widths
     # Initial number of particles
     initial_particles = (10**-12/14)*6.022*10**23
     # t(1/2) half life in years
     half_life = 5700
-    # Decay constant
+    # 2* t(1/2) half life in years
     half_life_2 = 5700*2
+    # Decay constant
     tau = -half_life/math.log(0.5)
-    time = []
-    number_particle_at_t =[]
-    rate = []
-    
+    time = []  # List for storing time
+    number_particle_at_t = []  # List for number of particles
+    rate = []  # List for storing rate
+    '''For loop for cyclying though each step width'''
     for i in range(len(step_size)):
+        # Emptying the list
         del time[:]
         del number_particle_at_t[:]
         del rate[:]
-
-        for t in range(0,20000,step_size[i]):
+        '''For loop for cyclying though each time stamp'''
+        for t in range(0, 20000, step_size[i]):
             if t == 0:
-                number_particle_at_t.append(initial_particles)
-                time.append(t)
+                number_particle_at_t.append(
+                    initial_particles)  # Adding the t=o value
+                time.append(t)  # Adding the t=o value
             else:
+                # Calculating the index value for the list
                 index = int((t)/step_size[i])
-                number_particle_at_t.append(number_particle_at_t[index-1]-(1/tau*number_particle_at_t[index-1]*step_size[i]))
-                #print(number_particle_at_t[index],number_particle_at_t[index-1])
-                rate.append(-(number_particle_at_t[index]-number_particle_at_t[index-1])/step_size[i])
-                time.append(t)
-            if step_size[i] == 1000: 
+                number_particle_at_t.append(number_particle_at_t[index-1]-(
+                    1/tau*number_particle_at_t[index-1] *
+                    step_size[i]))  # adding the next value
+                # adding the next value
+                rate.append(-(number_particle_at_t[index] -
+                            number_particle_at_t[index-1])/step_size[i])
+                time.append(t)  # adding the next value
+            if step_size[i] >= 1000:  # checking for a special condition of step width is 1000
+                # List for storing number of particle when 2nd order of taylor expansion is considered
                 no_2nd_order = []
-                time_2nd_order =[]
-                rate_2nd_order = []
-                for t in range(0,20000,step_size[i]):
+                time_2nd_order = []  # List for storing time
+                rate_2nd_order = []  # List for storing rate
+                for t in range(0, 20000, step_size[i]):
                     if t == 0:
                         no_2nd_order.append(initial_particles)
                         time_2nd_order.append(t)
                     else:
                         index = int((t)/step_size[i])
-                        no_2nd_order.append(no_2nd_order[index-1]-(1/tau*no_2nd_order[index-1]*step_size[i])+(1/tau**2*no_2nd_order[index-1]*step_size[i]**2)/2)
-                        #print(number_particle_at_t[index],number_particle_at_t[index-1])
-                        rate_2nd_order.append(-(no_2nd_order[index]-no_2nd_order[index-1])/step_size[i])
+                        no_2nd_order.append(no_2nd_order[index-1] -
+                                            (1/tau*no_2nd_order[index-1]*step_size[i]) +
+                                            (1/tau**2*no_2nd_order[index-1]*step_size[i]**2)/2)  # Considering the 2nd order term of the taylor expansion
+                        rate_2nd_order.append(
+                            -(no_2nd_order[index]-no_2nd_order[index-1])/step_size[i])
                         time_2nd_order.append(t)
-                    
 
+        plt.plot(time[:-1], rate, label=" Step_Width = " +
+                 step_[i])  # Plotting normal condition
+        if step_size[i] == 1000:  # Plotting special condition
+            plt.plot(time_2nd_order[:-1], rate_2nd_order,
+                     label=" Step_Width = "+step_[i] + " 2nd order considered")
+        # interpolating the value of rate
+        rate_interp = round(np.interp(half_life_2, time[:-1], rate), 2)
+        print("Rate at " + str(half_life_2) + " years = " +
+              str(rate_interp) + " ;for step width = " + str(step_size[i]))
+        if step_size[i] == 1000:
+            # interpolating the value of rate
+            rate_interp = round(
+                np.interp(half_life_2, time_2nd_order[:-1], rate_2nd_order), 2)
+            print("Rate at " + str(half_life_2) + " years = " + str(rate_interp) +
+                  " ;for step width = " + str(step_size[i])+" considering the 2nd order")
 
-        plt.plot(time[:-1], rate, label=" Step_Width = "+step_[i])
-        if step_size[i] == 1000: 
-            plt.plot(time_2nd_order[:-1], rate_2nd_order, label=" Step_Width = "+step_[i]+ " 2nd order considered")
-        rate_interp = round(np.interp(half_life_2, time[:-1], rate),2) 
-        print("Rate at "+ str(half_life_2) +" years = "+ str(rate_interp) + " ;for step width = "+ str(step_size[i]))
-        if step_size[i] == 1000: 
-            rate_interp = round(np.interp(half_life_2, time_2nd_order[:-1], rate_2nd_order),2)
-            print("Rate at "+ str(half_life_2) +" years = "+ str(rate_interp) + " ;for step width = "+ str(step_size[i])+" considering the 2nd order") 
-
-       
-
-    # print(number_particle_at_t)
     plt.xlabel("Time (Years)")
     plt.ylabel("Rate * 10^6 (Number of particles/years)")
     plt.legend()
@@ -74,9 +96,4 @@ def beta_decay():
 
 
 if __name__ == "__main__":
-    beta_decay()
-
-
-
-
-
+    beta_decay()  # Calling the function
--- HW2/golf.py
+++ HW2/golf.py
@@ -1,157 +1,198 @@
+# Import matplotlib.pyplot for plotting the graph
+# import numpy for the array of x-axis and the cos(x)/sin(x)/sinc(x)
+# Impprt math for doing mathermatical operations
+# Import argparse for passing arguments from the terminal to python code
 import numpy as np
 import matplotlib.pyplot as plt
 import math
 import argparse as argp
 
-
-
-v = 40
-dt = 0.005
-x_trajec = []
-y_trajec = []
-x_trajec_d = []
-y_trajec_d = []
-x_trajec_dd = []
-y_trajec_dd = []
-x_trajec_dds = []
-y_trajec_dds = []
-g = 9.81
-vx = []
-vy = []
-vx_d = []
-vy_d = []
-vx_dd = []
-vy_dd = []
-vx_dds = []
-vy_dds = []
-d = 1.29
-A = 0.0014
-m = 0.046
-
+v = 40  # Initial velocity,m/s
+dt = 0.005  # step size,s
+x_trajec = []  # Ideal x-axis trajectory
+y_trajec = []  # Ideal y-axis trajectory
+x_trajec_d = []  # drag x-axis trajectory
+y_trajec_d = []  # drag y-axis trajectory
+x_trajec_dd = []  # dimpled+spin x-axis trajectory
+y_trajec_dd = []  # dimpled+spin y-axis trajectory
+x_trajec_dds = []  # drag+dimpled+spin x-axis trajectory
+y_trajec_dds = []  # drag+dimpled+spin y-axis trajectory
+g = 9.81  # Accelarationn due to gravity
+vx = []  # Ideal x-axis velocity
+vy = []  # Ideal y-axis velocity
+vx_d = []  # drag x-axis velocity
+vy_d = []  # drag y-axis velocity
+vx_dd = []  # dimpled+spin x-axis velocity
+vy_dd = []  # dimpled+spin y-axis velocity
+vx_dds = []  # drag+dimpled+spin x-axis velocity
+vy_dds = []  # drag+dimpled+spin y-axis velocity
+d = 1.29  # Density of the air, kg/m^3
+A = 0.0014  # Area of the ball, m^2
+m = 0.046  # Mass of the ball, kg
 
 
 def golf_trajectory():
+    """ This functions take argument --plot=angle.The code calculate
+    the trajectory of a golf ball and plot it for the conditions like
+    ideal, smooth ball with drag, dimpled ball with drag and dimpled ball
+    with drag + spin """
+
     psr = argp.ArgumentParser("Golf_Trajectory")
     psr.add_argument('--plot', type=str, default=0,
                      help="enter the value of theta seperated by ','")
     arg = psr.parse_args()
     angle = arg.plot.split(",")
-    theta = [int(angl) for angl in angle] # check this again
+    theta = [int(angl) for angl in angle]  # converting the angles to integer
     plt.figure(figsize=(9, 7))
-
-    for l in range(4):
+    '''For loop for cyclying though each conditons
+    and through all the given angles'''
+    for condition in range(4):
         for k in range(len(theta)):
-            if l == 0:
+            if condition == 0:
+                # deleting the values to resure the same variable
                 del x_trajec[:]
                 del y_trajec[:]
                 del vx[:]
                 del vy[:]
-                x_trajec.append(0)
-                y_trajec.append(0)
+                x_trajec.append(0)  # Adding the t=o value
+                y_trajec.append(0)  # Adding the t=o value
+                # Adding the t=o value
                 vx.append(v*math.cos(math.radians(theta[k])))
+                # Adding the t=o value
                 vy.append(v*math.sin(math.radians(theta[k])))
                 i = 0
                 while True:
+                    # Appeding the next value
                     x_trajec.append(x_trajec[i]+vx[i]*dt)
-                    vx.append(vx[i])
-
+                    vx.append(vx[i])  # Appeding the next value
+                    # Appeding the next value
                     y_trajec.append(y_trajec[i]+vy[i]*dt)
-                    vy.append(vy[i]-g*dt)
-
+                    vy.append(vy[i]-g*dt)  # Appeding the next value
+                    
+                    # Checking if the ball has touched the ground or not
                     if y_trajec[i+1] <= 0:
+                    
                         break
 
                     i = i+1
 
-                plt.plot(x_trajec[0:i-1], y_trajec[0:i-1],':', label=str(theta[k])+"°"+"(Ideal)")
+                plt.plot(x_trajec[0:i-1], y_trajec[0:i-1],
+                         ':', label=str(theta[k])+"°"+"(Ideal)")
 
-            if l == 1:
-                
+            if condition == 1:
+                # deleting the values to resure the same variable
                 del x_trajec_d[:]
                 del y_trajec_d[:]
                 del vx_d[:]
                 del vy_d[:]
-                x_trajec_d.append(0)
-                y_trajec_d.append(0)
+                x_trajec_d.append(0)  # Adding the t=o value
+                y_trajec_d.append(0)  # Adding the t=o value
+                # Adding the t=o value
                 vx_d.append(v*math.cos(math.radians(theta[k])))
+                # Adding the t=o value
                 vy_d.append(v*math.sin(math.radians(theta[k])))
                 i = 0
                 while True:
+                    # Appeding the next value
                     x_trajec_d.append(x_trajec_d[i]+vx_d[i]*dt)
                     vx_d.append(
-                        vx_d[i]+(-1*0.5*d*A*math.sqrt(vx_d[i]**2+vy_d[i]**2)*vx_d[i]*dt)/m)
+                        vx_d[i]+(-1*0.5*d*A*math.sqrt(vx_d[i]**2+vy_d[i]**2)
+                                 *vx_d[i]*dt)/m)  # Appeding the next value
+                    # Appeding the next value
                     y_trajec_d.append(y_trajec_d[i]+vy_d[i]*dt)
-                    vy_d.append(vy_d[i]-g*dt+(-1*0.5*d*A*math.sqrt(vx_d[i]**2+vy_d[i]**2)*vx_d[i]*dt)/m)
+                    vy_d.append(
+                        vy_d[i]-g*dt+(-1*0.5*d*A*math.sqrt(vx_d[i]**2+vy_d[i]**2)
+                                      *vx_d[i]*dt)/m)  # Appeding the next value
 
+                    # Checking if the ball has touched the ground or not
                     if y_trajec_d[i+1] <= 0:
                         break
                     i = i+1
-                
-                plt.plot(x_trajec_d[0:i-1], y_trajec_d[0:i-1],'-.',
+
+                plt.plot(x_trajec_d[0:i-1], y_trajec_d[0:i-1], '-.',
                          label=str(theta[k])+"°"+"(Drag)")
-                # print(y_trajec_d[i],y_trajec_d[i-1],y_trajec_d[i-2])
 
-
-            
-            if l == 2:
+            if condition == 2:
+                # deleting the values to resure the same variable
                 del x_trajec_dd[:]
                 del y_trajec_dd[:]
                 del vx_dd[:]
                 del vy_dd[:]
-                x_trajec_dd.append(0)
-                y_trajec_dd.append(0)
+                x_trajec_dd.append(0)  # Adding the t=o value
+                y_trajec_dd.append(0)  # Adding the t=o value
+                # Adding the t=o value
                 vx_dd.append(v*math.cos(math.radians(theta[k])))
+                # Adding the t=o value
                 vy_dd.append(v*math.sin(math.radians(theta[k])))
                 i = 0
                 while True:
+                    # Appeding the next value
                     x_trajec_dd.append(x_trajec_dd[i]+vx_dd[i]*dt)
+                    # Appeding the next value
                     y_trajec_dd.append(y_trajec_dd[i]+vy_dd[i]*dt)
-                    if math.sqrt(vx_dd[i]**2+vy_dd[i]**2) >14:
+                    # Checking for the condition to select optimum C value
+                    if math.sqrt(vx_dd[i]**2+vy_dd[i]**2) > 14:
+                        # Appeding the next value
                         vx_dd.append(vx_dd[i]+(-1*7*d*A*vx_dd[i]*dt)/m)
+                        # Appeding the next value
                         vy_dd.append(vy_dd[i]-g*dt+(-1*7*d*A*vy_dd[i]*dt)/m)
-                    else: 
-                        vx_dd.append(vx_dd[i]+(-1*0.5*d*A*math.sqrt(vx_dd[i]**2+vy_dd[i]**2)*vx_dd[i]*dt)/m)
-                        vy_dd.append(vy_dd[i]-g*dt+(-1*0.5*d*A*math.sqrt(vx_dd[i]**2+vy_dd[i]**2)*vy_dd[i]*dt)/m)
-                    
+                    else:
+                        vx_dd.append(
+                            vx_dd[i]+(-1*0.5*d*A*math.sqrt(vx_dd[i]**2+vy_dd[i]**2)
+                                      * vx_dd[i]*dt)/m)  # Appeding the next value
+                        vy_dd.append(
+                            vy_dd[i]-g*dt+(-1*0.5*d*A*math.sqrt(vx_dd[i]**2+vy_dd[i]**2)
+                                           * vy_dd[i]*dt)/m)  # Appeding the next value
+
+                    # Checking if the ball has touched the ground or not
                     if y_trajec_dd[i+1] <= 0:
                         break
                     i = i+1
 
-                plt.plot(x_trajec_dd[0:i-1], y_trajec_dd[0:i-1],'--',
+                plt.plot(x_trajec_dd[0:i-1], y_trajec_dd[0:i-1], '--',
                          label=str(theta[k])+"°"+"(Dimpled + Drag)")
-                # plt.annotate(f'({x_trajec_dd[i-1]:.2f}, {y_trajec_dd[i-1]:.2f})', (x_trajec_dd[i-1], y_trajec_dd[i-1]), fontsize=3,xytext=(-5, 5), textcoords='offset points',
-                #  arrowprops=dict(arrowstyle='->', connectionstyle='arc3,rad=0.5'))
-                # print(y_trajec_dd[i],y_trajec_dd[i-1],y_trajec_dd[i-2])
 
-            if l == 3:
+            if condition == 3:
+                # deleting the values to resure the same variable
                 del x_trajec_dds[:]
                 del y_trajec_dds[:]
                 del vx_dds[:]
                 del vy_dd[:]
-                x_trajec_dds.append(0)
-                y_trajec_dds.append(0)
+                x_trajec_dds.append(0)  # Adding the t=o value
+                y_trajec_dds.append(0)  # Adding the t=o value
+                # Adding the t=o value
                 vx_dds.append(v*math.cos(math.radians(theta[k])))
+                # Adding the t=o value
                 vy_dds.append(v*math.sin(math.radians(theta[k])))
                 i = 0
                 while True:
+                    # Appeding the next value
                     x_trajec_dds.append(x_trajec_dds[i]+vx_dds[i]*dt)
+                    # Appeding the next value
                     y_trajec_dds.append(y_trajec_dds[i]+vy_dds[i]*dt)
-                    if math.sqrt(vx_dds[i]**2+vy_dds[i]**2) >14:
-                        vx_dds.append(vx_dds[i]+(-1*7*d*A*vx_dds[i]*dt)/m-0.25*vy_dds[i]*dt)
-                        vy_dds.append(vy_dds[i]-g*dt+(-1*7*d*A*vy_dds[i]*dt)/m+0.25*vx_dds[i]*dt)
-                    else: 
-                        vx_dds.append(vx_dds[i]+(-1*0.5*d*A*math.sqrt(vx_dds[i]**2+vy_dds[i]**2)*vx_dds[i]*dt)/m-0.25*vy_dds[i]*dt*dt)
-                        vy_dds.append(vy_dds[i]-g*dt+(-1*0.5*d*A*vy_dds[i]*dt)/m+0.25*vx_dds[i]*dt)
-        
-        
+                    # Checking for the condition to select optimum C value
+                    if math.sqrt(vx_dds[i]**2+vy_dds[i]**2) > 14:
+                        vx_dds.append(
+                            vx_dds[i]+(-1*7*d*A*vx_dds[i]*dt)/m-0.25
+                            * vy_dds[i]*dt)  # Appeding the next value
+                        vy_dds.append(
+                            vy_dds[i]-g*dt+(-1*7*d*A*vy_dds[i]*dt)/m+0.25
+                            * vx_dds[i]*dt)  # Appeding the next value
+                    else:
+                        vx_dds.append(
+                            vx_dds[i]+(-1*0.5*d*A*math.sqrt(vx_dds[i]**2+vy_dds[i]**2)
+                                       * vx_dds[i]*dt)/m-0.25*vy_dds[i]*dt*dt)  # Appeding the next value
+                        vy_dds.append(
+                            vy_dds[i]-g*dt+(-1*0.5*d*A*vy_dds[i]*dt)/m+0.25
+                            * vx_dds[i]*dt)  # Appeding the next value
+
+                    # Checking if the ball has touched the ground or not
                     if y_trajec_dds[i+1] <= 0:
                         break
                     i = i+1
 
-
-                plt.plot(x_trajec_dds[0:i-1], y_trajec_dds[0:i-1],'-',
+                plt.plot(x_trajec_dds[0:i-1], y_trajec_dds[0:i-1], '-',
                          label=str(theta[k])+"°"+"(Spin + Dimpled + Drag)")
-
 
     plt.xlabel("Distance (m)")
     plt.ylabel("Height (m)")
@@ -159,6 +200,7 @@ def golf_trajectory():
     plt.title("Trajectory of golf ball")
     plt.savefig("Golf_Trajectory.jpeg")
     plt.show()
-    
+
+
 if __name__ == "__main__":
-    golf_trajectory()
+    golf_trajectory()  # calling the function
